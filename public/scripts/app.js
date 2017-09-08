e01ee64794f4c735b79b521adc9de624e9b368f0
Shayne Meyer <shayne.meyer@gmail.com>
2017-09-08 12:25:29 -0700

    basic react setup using CDNs and createElement

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+console.log('app running');
+
+var template = React.createElement(
+  "h1",
+  { id: "someid"},
+  "Something new"
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
+
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 # Indecision App
+
+## Run local server
+```
+live-server public
+```
