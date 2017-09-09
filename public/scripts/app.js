bc72a41b221c122e682e37e52c62f5f4de255740
Shayne Meyer <shayne.meyer@gmail.com>
2017-09-08 17:01:52 -0700

    made some templates using jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,59 @@
-'use strict';
+"use strict";
 
 console.log('app running');
 
+// create app object title/subtitle
+// use title/subtitle in the template
+// render template
+
+var app = {
+  title: "Indecision App",
+  subtitle: "This is some info"
+};
+
 var template = React.createElement(
-  'h1',
+  "div",
   null,
-  'This is JSX'
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.subtitle
+  )
 );
+
+var user = {
+  name: "Shayne",
+  age: "?",
+  location: "Seattle, WA."
+};
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.name
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: ",
+    user.age
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: ",
+    user.location
+  )
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,35 @@
 console.log('app running');
 
-var template = <h1>This is JSX</h1>;
+// create app object title/subtitle
+// use title/subtitle in the template
+// render template
+
+var app = {
+  title: "Indecision App",
+  subtitle: "This is some info"
+};
+
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+  </div>
+);
+
+var user = {
+  name: "Shayne",
+  age: "?",
+  location: "Seattle, WA."
+};
+
+var templateTwo = (
+  <div>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
+  </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
