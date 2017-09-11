1d810e2d9a5da92e75bb924f759acde9adf05f27
Shayne Meyer <shayne.meyer@gmail.com>
2017-09-11 07:49:05 -0700

    events and attributes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -49,23 +49,45 @@ function getLocation(location) {
   }
 }
 
+var count = 0;
+var addOne = function addOne() {
+  console.log('addOne');
+};
+
+var minusOne = function minusOne() {
+  console.log('minusOne');
+};
+
+var resetButton = function resetButton() {
+  console.log('reset');
+};
+
 var templateTwo = React.createElement(
   "div",
   null,
   React.createElement(
     "h1",
     null,
-    user.name ? user.name : 'Anonymous'
+    "Count: ",
+    count
   ),
-  (user.age && user.age) >= 18 && React.createElement(
-    "p",
-    null,
-    "Age: ",
-    user.age
+  React.createElement(
+    "button",
+    { onClick: addOne },
+    "+1"
   ),
-  getLocation(user.location)
+  React.createElement(
+    "button",
+    { onClick: minusOne },
+    "-1"
+  ),
+  React.createElement(
+    "button",
+    { onClick: resetButton },
+    "reset"
+  )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
