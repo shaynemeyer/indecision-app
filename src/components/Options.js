1920f8a609e68a21508714b5e8cc47aff14a5cf3
Shayne Meyer <shayne.meyer@gmail.com>
2017-09-15 17:25:56 -0700

    added widget message style

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,10 +3,14 @@ import Option from './Option';
 
 const Options = ({options, handleDeleteOptions, handleDeleteOption }) => (
     <div>
-      <button
-        className="button button--link"
-        onClick={handleDeleteOptions}
-      >Remove all</button>
+      <div className="widget-header">
+        <h3 className="widget-header__title">Your Options</h3>
+        <button
+          className="button button--link"
+          onClick={handleDeleteOptions}
+        >Remove all</button>
+      </div>
+      {options.length === 0 && <p className="widget__message">Please add an option to get started!</p> }
       {
         options.map(option => (
           <Option
